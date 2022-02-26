a837085a648c9ae31a3294abf7eefce8b2fdeeb7
kokrui <kokruiwong@gmail.com>
2022-02-26 22:38:36 +0800

    harsher punishments

--- gmail.js
+++ gmail.js
@@ -5,8 +5,11 @@ function findToCc(){
             emails = elem.querySelectorAll("div.vR");
             if(emails.length>0){
                 alert("o(｀ω´ )o BCC ONLY!");
+                chrome.runtime.sendMessage({closeThis: true});
                 for(email of emails){
                     elem.removeChild(email);
+                    // beware >:)
+                    // this doesn't do what one might think it does xd
                 }
             }
         }
